073365ac2183e5cb2c84b1021d9cf07469ef8c9e
Dongmin, Yu <ydm2790@gmail.com>
2026-05-07 17:44:55 +0900

    chore: 🔨 configure development tools
    This commit externalizes and consolidates various development tool configurations into dedicated files.
    
    *   Moves Prettier configuration from `package.json` to `.prettierrc.mjs`.
    *   Moves `release-it` configuration from `package.json` to `.release-it.json`.
    *   Moves `react-native-builder-bob` configuration from `package.json` to `bob.config.js`.
    *   Moves Jest configuration from `package.json` to `jest.config.js`.
    *   Updates ESLint configuration to ignore `.yarn/`, `eslint.config.mjs`, and `lib/` directories, and to enable proper parsing for `.mjs` files.
    
    This improves project organization and maintainability by centralizing tool settings and decluttering `package.json`.

---
+++ .prettierrc.mjs
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  quoteProps: 'consistent',
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+};
+
+export default config;
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  preset: '@react-native/jest-preset',
+  modulePathIgnorePatterns: [
+    '<rootDir>/example/node_modules',
+    '<rootDir>/lib/',
+  ],
+};
